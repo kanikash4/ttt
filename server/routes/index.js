984ec78d2efa868d0883f31baf183df271aa8dfa
Kanika Sharma <kanikash4@gmail.com>
2017-11-11 23:30:45 +0530

    Server side additions

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,11 +2,14 @@
 
 var error = require('./error');
 var respond = require('./respond');
+var file = require('./fetchFile');
 
 module.exports = function(app) {
 
   app.all('/_status', function(req, res, next) {
     res.sendStatus(200);
   });
+
+  app.post('/submitNumber', file.fetchTTTFile, error, respond);
 
 };
